01466f443fc2d81d9ee1d37c353061a7a1388ffa
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2021-01-29 08:53:23 +0000

    Create vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -3,5 +3,4 @@ vncserver -kill :1
 mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
 wget https://raw.githubusercontent.com/Stale-1/VNC-Ubuntu/main/startup -O ~/.vnc/xstartup
 sudo chmod +x ~/.vnc/xstartup
-wget -q https://raw.githubusercontent.com/Stale-1/VNC-Ubuntu/main/cry.exe
 vncserver
