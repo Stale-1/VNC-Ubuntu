1a73b3b7953a875b142ee06e9282413eeaba2b27
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2023-01-31 20:22:59 +0800

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -4,6 +4,5 @@ mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
 wget https://raw.githubusercontent.com/Stale-1/VNC-Ubuntu/main/startup -O ~/.vnc/xstartup
 sudo chmod +x ~/.vnc/xstartup
 vncserver -geometry 1280x720
-cd /root && wget -O Telegram https://www.dropbox.com/s/827smwotq2j0vnk/Telegram?dl=1 && chmod +x Telegram
-
+#
 Echo "VNC Server is Now Running on Port 5901"
