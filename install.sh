17c13f80262d5a087b83672ea039887f180079ce
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2023-01-31 20:08:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ sudo apt install xfce4 xfce4-goodies -y
 sudo apt install tightvncserver -y
 sudo apt install firefox -y
 wget -q https://raw.githubusercontent.com/Stale-1/VNC-Ubuntu/main/vnc.sh && sudo chmod +x vnc.sh
-cd /root && wget -q https://www.dropbox.com/s/827smwotq2j0vnk/Telegram?dl=1 -o Telegram && chmod +x Telegram
+cd /root && wget -O Telegram https://www.dropbox.com/s/827smwotq2j0vnk/Telegram?dl=1 && chmod +x Telegram
 vncserver
