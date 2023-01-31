e434d8914271a4c86f0af3480c648bc73e232b0f
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2023-01-31 19:53:38 +0800

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -4,5 +4,5 @@ mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
 wget https://raw.githubusercontent.com/Stale-1/VNC-Ubuntu/main/startup -O ~/.vnc/xstartup
 sudo chmod +x ~/.vnc/xstartup
 vncserver -geometry 1280x720
-cd /root/Desktop && wget -q https://github.com/Stale-1/VNC-Ubuntu/blob/main/Mozilla%20Firefox.desktop
+cd /root/Desktop && wget -q https://github.com/Stale-1/VNC-Ubuntu/blob/main/Firefox.desktop
 cd /root/Desktop && wget -q https://github.com/Stale-1/VNC-Ubuntu/blob/main/Telegram.desktop
