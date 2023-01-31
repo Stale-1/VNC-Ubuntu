bad675d7a810038ee6c29584408dfd7553778ff1
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2023-01-31 18:07:42 +0800

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -3,4 +3,4 @@ vncserver -kill :1
 mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
 wget https://raw.githubusercontent.com/Stale-1/VNC-Ubuntu/main/startup -O ~/.vnc/xstartup
 sudo chmod +x ~/.vnc/xstartup
-vncserver
+vncserver -geometry 1280x720
